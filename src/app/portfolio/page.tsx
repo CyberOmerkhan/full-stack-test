69921be6d35268060bff0e14f03b29c99d9491f4
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-02-24 16:29:42 -0500

    page update

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link'
+import styles from './page.module.css'
+
 export default function Portfolio(){
     return (
-        <div>
-            It is a portfolio page
+        <div className = {styles.container}>
+            <h1 className = {styles.selectTitle}>Choose a gallery</h1>
+            <div className={styles.items}>
+                <Link href = 'portfolio/illustrations' className={styles.item}>
+                    <span className = {styles.title}>Illustrations</span>
+                </Link>
+                <Link href = 'portfolio/websites' className={styles.item}>
+                    <span className = {styles.title}>Websites</span>
+                </Link>
+                <Link href = 'portfolio/application' className={styles.item}>
+                    <span className = {styles.title}>Applications</span>
+                </Link>
+            </div>
         </div>
     )
 }
