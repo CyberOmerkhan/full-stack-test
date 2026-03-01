d51090bedc2a0795eaf218f5c64b19dedc43a755
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-02-28 20:20:34 -0500

    additional mod

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,10 +1,14 @@
+"use client"
+
 import React from 'react'
 import styles from './DarkModeToggle.module.css'
+import {ThemeContext} from '@/context/ThemeContext'
+import {useContext} from 'react'
 
 export default function DarkModeToggle() {
-    const mode = "dark"
+    const { mode, toggle } = useContext(ThemeContext);
   return (
-    <div className = {styles.container}>
+    <div className = {styles.container} onClick = {toggle}>
       <div className = {styles.icon}>
         🌙
       </div>
